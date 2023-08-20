1385bc15bd416775721bacad7cb795bcc3dc0962
Hamid <psyneex@gmail.com>
2023-08-21 01:50:18 +0330

    initialcommit

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const textGeneration = async (prompt) => {
             model: 'text-davinci-003',
             prompt: `Human: ${prompt}\nAI: `,
             temperature: 0.9,
-            max_tokens: 400,
+            max_tokens: 500,
             top_p: 1,
             frequency_penalty: 0,
             presence_penalty: 0.6,
